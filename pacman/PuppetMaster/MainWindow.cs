da910f4c3d02189f77331ed58f6a17f5235c53c8
GoncaloPina <goncalopina10@gmail.com>
2017-12-06 20:23:37 +0000

    add form for PM

--- pacman/PuppetMaster/MainWindow.cs
+++ pacman/PuppetMaster/MainWindow.cs
@@ -22,5 +22,104 @@ namespace pacman
         {
            // AllocConsole();
         }
+
+        private void btn_crash_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_freeze_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void btn_wait_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_local_state_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void btn_global_status_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_load_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_runcommand_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void btn_start_server_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_start_client_Click(object sender, EventArgs e)
+        {
+
+        }
+
+       
+
+        private void btn_unfreeze_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+
+        private void btn_run_command_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_next_command_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_delay_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_localstate_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+
+        private void btn_globalstate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void output_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void file_box_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
